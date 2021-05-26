1551c73a73aa7f9d6c53bca5affc4950b2ade435
tjzetty <65916505+tjzetty@users.noreply.github.com>
2021-05-26 10:06:18 -0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,71 @@
+import gcsa.recurrence
+from gcsa.recurrence import _DayOfTheWeek
+import datetime
+import calendar
+
+
+def convertMonthToNum(month):
+    x = 0
+    if month == "Jan":
+        x = 1
+    elif month == "Feb":
+        x = 2
+    elif month == "Mar":
+        x = 3
+    elif month == "Apr":
+        x = 4
+    elif month == "May":
+        x = 5
+    elif month == "Jun":
+        x = 6
+    elif month == "Jul":
+        x = 7
+    elif month == "Aug":
+        x = 8
+    elif month == "Sep":
+        x = 9
+    elif month == "Oct":
+        x = 10
+    elif month == "Nov":
+        x = 11
+    else:
+        x = 12
+    return x
+
+def convertDaysToList(days):
+    list = {}
+    count = 0
+    for char in days:
+        if char == 'M':
+            list[count] = gcsa.recurrence.MONDAY
+        elif char == 'T':
+            list[count] = gcsa.recurrence.TUESDAY
+        elif char == 'W':
+            list[count] = gcsa.recurrence.WEDNESDAY
+        elif char == 'R':
+            list[count] = gcsa.recurrence.THURSDAY
+        elif char == 'F':
+            list[count] = gcsa.recurrence.FRIDAY
+        count = count + 1
+    return list
+
+def dayToNum(day):
+    num = 0
+    if day[0] == 'T':
+        num = 1
+    elif day[0] == 'W':
+        num = 2
+    elif day[0] == 'R':
+        num = 3
+    elif day[0] == 'F':
+        num = 4
+    return num
+
+def findDay(beginSemester, startClass):
+    temp = datetime.datetime.strptime(beginSemester, '%d %m %Y').weekday()
+    y = calendar.day_name[temp]
+    ynum = dayToNum(y)
+    xnum = dayToNum(startClass)
+    if ynum > xnum:
+        return 7 - (ynum - xnum)
+    return xnum - ynum
---
+++ main.py
@@ -0,0 +1,111 @@
+import re
+from datetime import datetime, timedelta
+from gcsa.event import Event
+from gcsa.google_calendar import GoogleCalendar
+from gcsa.recurrence import *
+from functions import convertMonthToNum, convertDaysToList, findDay
+
+# open banweb detail class schedule and calendar
+inputFile = open("G:\\My Drive\\xNot Classes\\Projects\\autoCalendar\\fall21schedule.txt", "r")
+calendar = GoogleCalendar('tjzetty@mtu.edu')
+
+# initialize variables
+courseName = {}
+courseNumber = {}
+
+time = {}
+startTime = {}
+endTime = {}
+days = {}
+month = {}
+
+location = {}
+dateRange = {}
+professor = {}
+
+# HTML indicators to extract class info.
+findClass = '<table class="datadisplaytable" summary="This layout table is used to present the schedule course detail"><caption class="captiontext">'
+checkString = '<th class="ddheader" scope="col">Instructors</th>'
+
+data = 0
+index = -1
+# extract class info and add to calendar
+for line in inputFile:
+    if findClass in line:
+        index = index + 1
+        # extracting substrings for course name and number
+        course = re.findall('<caption class="captiontext">(.+)</caption>', line)[0]
+        courseName[index] = re.findall('(.*?) - ', course)[0]
+        courseNumber[index] = re.findall(' - (.*?) - (.*?)\Z', course)[0]
+        courseNumber[index] = courseNumber[index][0] + " " + courseNumber[index][1]
+    if checkString in line:
+        data = 1
+    if data > 0:
+        if data == 5:
+            time[index] = re.findall('<td class="dddefault">(.+)</td>', line)[0]
+        if data == 6:
+            days[index] = re.findall('<td class="dddefault">(.+)</td>', line)[0]
+        if data == 7:
+            location[index] = re.findall('<td class="dddefault">(.+)</td>', line)[0]
+        if data == 8:
+            dateRange[index] = re.findall('<td class="dddefault">(.+)</td>', line)[0]
+        if data == 9:
+            professor[index] = re.findall('<td class="dddefault">(.+)</td>', line)[0]
+            data = -1
+        data = data + 1
+
+# Date and Month Calculations:
+endRange = re.findall(' - (.*)', dateRange[0])[0]
+monthStart = convertMonthToNum(re.findall('\A(.*?) ', dateRange[0])[0])
+monthEnd = convertMonthToNum(re.findall(' - (.*?) ', dateRange[0])[0])
+dateStart = int(re.findall(' (.*?), ', dateRange[0])[0])
+dateEnd = int(re.findall(' (.*?),', endRange)[0])
+yearStart = int(re.findall(', (.*?) - ', dateRange[0])[0])
+yearEnd = int(re.findall(', (.+)', endRange)[0])
+
+for x in range(0, index+1):
+    startTime = 12
+    endTime = 12
+    hourStart = 0
+    hourEnd = 0
+    minStart = 0
+    minEnd = 0
+
+    paramFindDay = str(dateStart) + ' ' + str(monthStart) + ' ' + str(yearStart)
+    # Adjust start date from semester start to real class start
+    adjustDate = findDay(paramFindDay, days[x])
+
+    if time[x][0] != '<':
+        # Start Time Calculations: minutes, hours
+        startTime = re.findall('(.*) - ', time[x])[0]
+        if re.findall(' (.+)', startTime)[0][0] == 'p':
+            hourStart = int(re.findall('(.+):', startTime)[0]) + 12
+            minStart = int(re.findall(':(.+) p', startTime)[0])
+        if re.findall(' (.+)', startTime)[0][0] == 'a':
+            hourStart = int(re.findall('(.+):', startTime)[0])
+            minStart = int(re.findall(':(.+) a', startTime)[0])
+
+        # End Time Calculations: minutes, hours
+        endTime = re.findall(' - (.+)', time[x])[0]
+        if re.findall(' (.+)', endTime)[0][0] == 'p':
+            hourEnd = int(re.findall('(.+):', endTime)[0]) + 12
+            minEnd = int(re.findall(':(.+) p', endTime)[0])
+        else:
+            if re.findall(' (.+)', endTime)[0][0] == 'a':
+                hourEnd = int(re.findall('(.+):', endTime)[0])
+                minEnd = int(re.findall(':(.+) a', endTime)[0])
+
+    if hourEnd == 24:
+        hourEnd = 12
+    if hourStart == 24:
+        hourStart = 12
+
+    daysOfWeek = list(convertDaysToList(days[x]).values())
+    event = Event(
+        courseName[x],
+        start=datetime(year=yearStart, month=monthStart, day=dateStart, hour=hourStart, minute=minStart) + timedelta(adjustDate),
+        end=datetime(year=yearStart, month=monthStart, day=dateStart, hour=hourEnd, minute=minEnd) + timedelta(adjustDate),
+        minutes_before_popup_reminder=20,
+        recurrence=Recurrence.rule(freq=WEEKLY, until=date(year=yearEnd, month=monthEnd, day=dateEnd), by_week_day=daysOfWeek)
+    )
+    calendar.add_event(event)
